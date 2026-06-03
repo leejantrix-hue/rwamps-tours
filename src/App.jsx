dac42fb00c3ef26311d27477fc3b7a037ff12aee
leejantrix-hue <leejantrix@gmail.com>
2026-06-03 19:25:07 +0300

    fix(app): correct MainLayout import path

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import ScrollToTop from './helpers/ScrollToTop.jsx'
-import MainLayout from './helpers/MainLayout.jsx'
+import MainLayout from './layouts/MainLayout/MainLayout.jsx'
 import Home from './pages/Home/Home.jsx'
 import About from './pages/About/About.jsx'
 import Tours from './pages/Tours/Tours.jsx'
